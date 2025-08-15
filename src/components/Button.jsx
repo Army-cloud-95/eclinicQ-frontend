fc6bfcf5c32e2f5fec28e05a2d4ab38668c5eb94
kanak227 <kanakverma325@gmail.com>
2025-08-15 16:38:44 +0530

    Completed GetStarted page

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Button = ({buttonVariant , buttonText}) => {
+  return (
+    <div>
+      <button
+        className={`mt-5  w-full min-w-8 h-8 px-4 rounded-md font-medium transition text-sm ${
+          buttonVariant === "primary"
+            ? "bg-[#2372EC] text-white   hover:bg-blue-700"
+            : " text-[#424242] border-[0.5px] border-[#8E8E8E]"
+        }`}
+      >
+        {buttonText}
+      </button>
+    </div>
+  )
+}
+
+export default Button
